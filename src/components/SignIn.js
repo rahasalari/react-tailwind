bbb78f3a776da5fbaabe925212bd745b59e2e846
macbook <macbook@macbooks-MacBook-Pro.local>
2023-07-18 15:32:20 +0330

    create validation form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,41 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Validate } from "./Validate";
 
 function SignIn () {
-
+    //states
     const [ contact, setContact ] = useState({
-        name:'',
-        email:'',
-        phoneNumber:''
+        name : '',
+        email : '',
+        phoneNumber : '',
     });
+
+    const [ errors, setErrors ] = useState({});
+
+    useEffect(() => {
+        setErrors(Validate(contact))
+        console.log(errors)
+    }, [contact]);
 
     const changeHandler = (e) => {
         setContact({...contact, [e.target.name] : e.target.value })
     }
 
     const submitHandler = (e) => {
-        e.preventDefault()
-        console.log(contact)
+        e.preventDefault();
+        console.log(contact);
+        // setErrors(validationForm(contact));
     }
 
     return(
-        <div>
-            <p>Contact</p>
+        <div class='mt-20 ms-20'>
+            <p class='mt-5'>Contact</p>
             <form onSubmit={submitHandler}>
                 <div>
                     <label>name</label>
                     <input type="text" name='name' value={contact.name} onChange={changeHandler} class="border pt-3 px-12  mx-auto placeholder-gray-400 placeholder:text-sm"/>
+                    {errors.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>email</label>
                     <input type="text" name='email' value={contact.email} onChange={changeHandler} class="border pt-3 px-12  mx-auto placeholder-gray-400 placeholder:text-sm"/>
+                    {errors.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>phone number</label>
                     <input type="text" name='phoneNumber' value={contact.phoneNumber} onChange={changeHandler} class="border pt-3 px-12  mx-auto placeholder-gray-400 placeholder:text-sm"/>
+                    {errors.phoneNumber && <span>{errors.phoneNumber}</span>}
                 </div>
                 <div class="">
-                    <button class="bg-transparent text-violet-700  py-2 px-6 border border-violet-700 text-xs mt-4">
-                        Let's Talk
+                    <button /*onClick={validationForm}*/ type="submit" class="bg-transparent text-violet-700  py-2 px-6 border border-violet-700 text-xs mt-4">
+                        Contact US
                     </button>
                 </div>
             </form>
---
+++ src/components/Validate.js
@@ -0,0 +1,33 @@
+export const Validate = contact => {
+    const errors = {};
+
+    if (!(contact.name.trim())) {
+        errors.name = 'username required';
+    } else if (!/^[A-Za-z]+$/.test(contact.name)){
+        errors.name = 'username should just have alphabet'
+    } else {
+        delete errors.name;
+    } //for mikhad ya na ?
+
+    if (!(contact.email)) {
+        errors.email = 'email required';
+    } else if (!/\S+@\S+\.\S+/.test(contact.email)) {
+        errors.email = 'email is invalid';
+    } else {
+        delete errors.email;
+    }
+
+
+    if (!(contact.phoneNumber)){
+        errors.phoneNumber = 'phoneNumber required'
+    } else if (!/^[0-9]+$/.test(contact.phoneNumber)){
+        errors.phoneNumber = 'the phone number is should be just numbers';
+    } else if(!(contact.phoneNumber.length === 11)) {
+        errors.phoneNumber = 'the phone number is not 11 numbers';
+    } else if (isNaN(contact.phoneNumber)){
+        // errors.phoneNumber = 'just numbers'
+    } else {
+        delete errors.phoneNumber
+    }
+    return errors;
+};
