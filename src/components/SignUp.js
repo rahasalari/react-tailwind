2266002a35066129e7bc3255cfdf9b53b960a662
macbook <macbook@macbooks-MacBook-Pro.local>
2023-07-19 13:33:22 +0330

    create onchange event with dispatch

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -13,8 +13,14 @@ const SignUp = () => {
     const phone = useSelector((store) => store.phone.phoneValue);
     //dispatch
     const dispatch = useDispatch();
-    const changeHandler = (e) => {
+    const nameChangeHandler = (e) => {
         dispatch(setName(e.target.value))
+    };
+    const emailChangeHandler = (e) => {
+        dispatch(setEmail(e.target.value))
+    };
+    const phoneChangeHandler = (e) => {
+        dispatch(setPhone(e.target.value))
     };
     //validation
     const [ errors, setErrors ] = useState({});
@@ -25,19 +31,19 @@ const SignUp = () => {
             <form class=''>
                 <div class='mt-5'>
                     <label class='text-sm'>name</label>
-                    <input type="text" name='name' value={name} onChange={changeHandler} class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-4"/>
+                    <input type="text" name='name' value={name} onChange={nameChangeHandler} class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-4"/>
                     <br></br>
                     {errors.name && <span class='text-red-500 text-lg ms-14'>{errors.name}</span>}
                 </div>
                 <div class='mt-6'>
                     <label class='text-sm'>email</label>
-                    <input type="text" name='email' class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-4"/>
+                    <input type="text" name='email' value={email} onChange={emailChangeHandler} class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-4"/>
                     <br></br>
                     {errors.email && <span class='text-red-500 text-lg ms-14'>{errors.email}</span>}
                 </div>
                 <div class='mt-6'>
                     <label class='text-sm'>phone</label>
-                    <input type="text" name='phoneNumber' class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-3"/>
+                    <input type="text" name='phoneNumber' value={phone} onChange={phoneChangeHandler} class="border pt-2 px-10  mx-auto placeholder-gray-400 placeholder:text-sm ms-3"/>
                     <br></br>
                     {errors.phoneNumber && <span class='text-red-500 text-lg ms-14'>{errors.phoneNumber}</span>}
                 </div>
